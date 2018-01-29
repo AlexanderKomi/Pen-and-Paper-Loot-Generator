bf0b365927faa5d6075eb6ee5c5016ca5ddcb8bb
niss <dsellemann@gmail.com>
2018-01-29 18:37:22 +0100

    added App logo

--- code/LootGenerator.java
+++ code/LootGenerator.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Map;
@@ -45,6 +46,13 @@ public class LootGenerator extends Application {
 		primaryStage.setMinHeight(665);
 		primaryStage.setScene( scene );
 		primaryStage.setOnCloseRequest( event -> Platform.exit() );
+
+		try {
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/graphics/PnPLogo.svg")));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
 		primaryStage.show();
 		
 		afterGuiInit();
